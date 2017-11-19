17b236d3edcef5864676ae2f3ee2a8af691c996a
Albin <info@albinso.com>
2017-11-19 23:16:40 +0100

    Made all mpd interactions go through the MpdController.

--- jukebox/views.py
+++ jukebox/views.py
@@ -1,12 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
-from django.views.decorators.cache import never_cache
-from . import models
-import subprocess
-
-# Create your views here.
-
+from . import mpd_instance
 
 def index(request):
     return HttpResponse("Hello from Partytown!")
@@ -17,19 +12,16 @@ def panel(request):
 
 
 def load(request):
-    playlist = get_object_or_404(models.PlayList, pk=request.POST['listID'])
-    playlist.load()
+    mpd_instance.load_playlist(request.POST['listID'])
     return HttpResponseRedirect(reverse('jukebox:panel'))
 
 
 def play(request):
-    command = models.make_mpc_command(['play'])
-    subprocess.call(command)
+    mpd_instance.play()
     return HttpResponseRedirect(reverse('jukebox:panel'))
 
 
 def pause(request):
-    command = models.make_mpc_command(['pause'])
-    subprocess.call(command)
+    mpd_instance.pause()
     return HttpResponseRedirect(reverse('jukebox:panel'))
 
