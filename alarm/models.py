4d2226e1f24775a3011b3c90b79358a0fae28f3c
Albin Söderholm <albin@albinso.com>
2020-05-10 03:37:05 +0200

    Creates Artist model that can be used to play random artist during alarm

--- alarm/models.py
+++ alarm/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Artist(models.Model):
+    name = models.CharField(max_length=255, default="")
+
+    def get_name(self):
+        return self.name
+
+    def __str__(self):
+        return self.name
+
+
+
--- alarm/tasks.py
+++ alarm/tasks.py
@@ -2,6 +2,7 @@ from albinso import celery_app
 import time
 from jukebox import mpd_instance
 import requests
+import random
 from .lightconfig import url
 
 
@@ -10,13 +11,22 @@ def alarm(self):
     lightemup()
     mpd_instance.set_volume(0)
     mpd_instance.pause()
+    queue_random_artist()
+    time.sleep(10)
     mpd_instance.play()
     for i in range(100):
         mpd_instance.raise_volume(1)
         time.sleep(2)
+
+def queue_random_artist():
+    from . import models
+    items = models.Artist.objects.all()
+    random_artist = random.choice(items)
+    mpd_instance.clear()
+    mpd_instance.search("artist", str(random_artist), 30)
     
 def lightemup():
-    for brightness in range(1, 256):
+    for brightness in range(250, 256):
         for i in range(1, 7):
             requests.get(url + "/light/bright?id={}&brightness={}".format(i, brightness))
             if brightness == 1:
